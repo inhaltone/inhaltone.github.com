3865c7e0f047626c1853c8b190c5a2c7834c7823
inhaltone <inhalt.xyz@gmail.com>
2023-04-04 22:30:12 +0300

    add: terminal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import TerminalText from "./components/TerminalText";
+import TerminalWindow from "./components/TerminalWindow";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                             d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429l4.243 4.242Z"/>
                     </svg>
                 </div>
+                <TerminalWindow />
                 <TerminalText text={"@inhaltone \n Front-End Developer with expertise in implementing SPAs utilizing modern JavaScript frameworks."}/>
             </div>
         </div>
---
+++ src/components/TerminalWindow.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import style from '../css/terminal.module.css';
+import IconClose from "./icons/IconClose";
+
+export default class TerminalWindow extends React.Component<any, any> {
+    public previousPosX: number;
+    public previousPosY: number;
+    public currentPosX: number;
+    public currentPosY: number;
+    private terminalWindowElement: HTMLElement;
+    private readonly terminalWindow: React.RefObject<HTMLDivElement>;
+
+    constructor(props: any) {
+        super(props);
+        this.previousPosX = 0;
+        this.previousPosY = 0;
+        this.currentPosX = 0;
+        this.currentPosY = 0;
+        this.terminalWindowElement = document.createElement('div');
+        this.terminalWindow = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.previousPosX);
+    }
+
+    drag(event: React.MouseEvent<HTMLElement>) {
+        this.terminalWindowElement = event.currentTarget;
+        this.previousPosX = event.clientX;
+        this.previousPosY = event.clientY;
+        document.onmousemove = (event) => {
+            this.currentPosX = this.previousPosX - event.clientX;
+            this.currentPosY = this.previousPosY - event.clientY;
+
+            this.previousPosX = event.clientX;
+            this.previousPosY = event.clientY;
+
+            this.terminalWindowElement.style.top = (this.terminalWindowElement.offsetTop - this.currentPosY) + 'px';
+            this.terminalWindowElement.style.left = (this.terminalWindowElement.offsetLeft - this.currentPosX) + 'px';
+        };
+        document.onmouseup = () => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+    close() {
+        this.terminalWindow.current?.classList.add('slide-out-blurred-bottom');
+    }
+
+
+    render() {
+        return (
+            <div ref={this.terminalWindow} onMouseDown={(event) => this.drag(event)} className={style.terminal}>
+                <div className={style.toolbox}>
+                    <div className={style.toolboxButtons}>
+                        <button onClick={() => this.close()} type="button" className={style.closeButton}><IconClose/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
