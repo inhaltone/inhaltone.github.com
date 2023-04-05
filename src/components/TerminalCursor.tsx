ea8c1779a16e92aa91b6dabcef4d53aff5411b50
inhaltone <inhalt.xyz@gmail.com>
2023-04-05 09:34:03 +0300

    add: refactor cursor component from class to function style

--- src/components/TerminalCursor.tsx
+++ src/components/TerminalCursor.tsx
@@ -1,39 +1,33 @@
 import React from "react";
 import styles from "../css/transition.module.css";
+import {useState, useEffect} from "react";
 
-export default class TerminalCursor extends React.Component<any, any> {
-    public cursor: boolean;
-    speed: number = 500;
+export default function TerminalCursor() {
+    const [cursor, setCursor] = useState('');
 
-    constructor(props: any) {
-        super(props);
-        this.state = {cursor: '▆'};
-        this.cursor = true;
-    }
+    useEffect(() => {
+        setCursor('▆');
+    }, [])
 
-    loop() {
-        setInterval(() => {
-            if (this.cursor) {
-                this.setState(() => ({
-                    cursor: "▆"
-                }));
-                this.cursor = false;
-            } else {
-                this.setState(() => ({
-                    cursor: ""
-                }));
-                this.cursor = true;
-            }
-        }, this.speed);
-    }
+    // const loop = () => {
+    //     setInterval(() => {
+    //         if (this.cursor) {
+    //             this.setState(() => ({
+    //                 cursor: "▆"
+    //             }));
+    //             this.cursor = false;
+    //         } else {
+    //             this.setState(() => ({
+    //                 cursor: ""
+    //             }));
+    //             this.cursor = true;
+    //         }
+    //     }, this.speed);
+    // }
 
-    componentDidMount() {
 
-    }
+    return (
+        <span className={styles.pulsate}>{cursor}</span>
+    )
 
-    render() {
-        return (
-            <span className={styles.pulsate}>{this.state.cursor}</span>
-        )
-    }
 }
